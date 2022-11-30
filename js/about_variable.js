1137837d89b3ab0ef1eff4a502def36f199dba4b
Igorplisko <igorplisko95@gmail.com>
2022-11-30 21:24:48 +0200

    innit

---
+++ js/about_variable.js
@@ -0,0 +1,52 @@
+let userName = 'Igor';
+
+const stringJson =
+  '{"name":"Igor","age":18,"info":{"isMale":true,"country":"Ukraine","city":"Kharkiv","smoke":false}}';
+
+let age = '30';
+
+let myAge = 30;
+
+let isMarried = false;
+
+let ageNull = null;
+
+//*undefined value
+
+let mySkills;
+// console.log(mySkills)   //undefined
+
+//*reset value
+
+let someName = 'James';
+
+someName = undefined; //better not to do that
+console.log(someName);
+
+someName = null;
+console.log(someName); //should be do like this
+
+//!---------------------------------
+//*Dynamic typing
+
+let someVariable = 'Some value';
+// console.log(someVariable)
+
+someVariable = 50;
+// console.log(someVariable)
+
+someVariable = true;
+// console.log(someVariable)
+
+//!---------------------------------
+//*this way of naming is bad
+
+let u = 'Vlad';
+let a = 30;
+
+//*good naming example
+
+let userLastName = 'Plisko';
+let userAge = 25;
+
+//!---------------------------------
