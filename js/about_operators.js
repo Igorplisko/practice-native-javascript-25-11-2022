2adbcdb2a65ed576fe4cf9a996fc190256c782e2
Igorplisko <igorplisko95@gmail.com>
2022-11-30 22:33:26 +0200

    innit

--- js/about_operators.js
+++ js/about_operators.js
@@ -5,30 +5,34 @@ let x = 5; //!
 
 //*  ==	 equal to
 
-let x1 = x == 8; //returns false
-let x2 = x == 5; //returns true
-let x3 = x == '5'; //returns true
+let result = x == 8; //returns false
+
+let result1 = x == 5; //returns true
+let result2 = x == '5'; //returns true
 
 //* ===	equal value and equal type
-let x4 = x === 5; //returns true
-let x5 = x === '5'; //returns false
+let result3 = x === 5; //returns true
+let result4 = x === '5'; //returns false
 
 //* !=	not equal
-let x6 = x != 8; //returns true
+let result5 = x != 8; //returns true
+let result6 = x != '5'; //returns false
 
 //* !==	not equal value or not equal type
-let x7 = x !== 5; //returns false
-let x8 = x !== '5'; //returns	true
-let x9 = x !== 8; //returns true
+let result7 = x !== 5; //returns false
+let result8 = x !== '5'; //returns	true
+let result9 = x !== 8; //returns true
 
 //* >	greater than
-let x10 = x > 8; //returns false
+let result10 = x > 8; //returns false
 
 //* <	less than
-let x11 = x < 8; //returns true
+let result11 = x < 8; //returns true
 
 //* >=	greater than or equal to
-let x12 = x >= 8; //returns false
+let result12 = x >= 8; //returns false
 
 //* <=	less than or equal to
-let x13 = x <= 8; //returns true
+let result13 = x <= 8; //returns true
+
+//!---------------------------------------
