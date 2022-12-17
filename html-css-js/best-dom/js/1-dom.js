0a3c1232ca58bf3e5a718b40b1a2f528fac95359
Igorplisko <igorplisko95@gmail.com>
2022-12-17 21:10:18 +0200

    commit

--- html-css-js/best-dom/js/1-dom.js
+++ html-css-js/best-dom/js/1-dom.js
@@ -4,42 +4,51 @@
 // Будет выбрать первый найденный элемент. Поиск осуществляется с начала кода страницы, сверху вниз.
 //document.querySelector('selector');
 
-//run this code in console
-document.querySelector('h2');  //возвращает первый элемент ( Element ) документа, который соответствует указанному селектору 
-document.querySelectorAll('h2')  //возвращает статический (не динамический) NodeList , содержащий все найденные элементы документа, которые соответствуют указанному селектору
+//!--------------------------------------------------------
+document.querySelector('h2').classList.add('red');
 
-const oneHeader = document.querySelector('h2');
-const header = document.querySelectorAll('h2')
+const heading = document.querySelector('h1');
+heading.classList.add('purple');
 
-console.log(oneHeader)
-console.log(header)
+const heading1 = document.querySelector('.heading-1');
+heading1.classList.add('green-text');
+heading1.classList.add('black');
 
+//нашли по классу класс и добавили другой класс
+document.querySelector('.heading-v2').classList.add('purple');
 
+//!--------------------------------------------------------
+//Выбор коллекции элементов
 
+const headingAll = document.querySelectorAll('h2');
+//headingAll.classList.add('red-text') //? ERROR can't read properties of undefined
 
+for (let headingOne of headingAll) {
+  // console.log(headingOne)
+  headingOne.classList.add('yellow-text');
+}
 
+//--------
 
+const paragraph = document.querySelectorAll('p');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const h2headings = document.querySelectorAll('h2');
-// for (heading of h2headings) {
-// 	console.log(heading);
-// 	heading.classList.add('green');
+// for (let p of paragraph ){
+//   p.classList.add('green-text'); 
 // }
 
+paragraph.forEach (function(item) {
+item.classList.add('sky-blue-text')
+})
 
+
+
+
+//run this code in console
+document.querySelector('h2'); //возвращает первый элемент ( Element ) документа, который соответствует указанному селектору
+document.querySelectorAll('h2'); //возвращает статический (не динамический) NodeList , содержащий все найденные элементы документа, которые соответствуют указанному селектор
+
+// console.log(oneHeader)
+// console.log(header)
 
 document.querySelector('h1'); //tag
 document.querySelector('.header'); //class
@@ -51,7 +60,8 @@ document.querySelector('#footer'); //id
 // const el = document.querySelector("div.user-panel.main input[name=login]");
 
 //? Также есть следующие методы
-// document.getElementsByClassName(); // выбор Коллекции элементов по CSS классу
-// document.getElementsByTagName(); // выбор Коллекции элементов по Тегу
-// document.getElementById(); // выбор одного элемента по ID
+// ? document.getElementsByClassName(); // выбор Коллекции элементов по CSS классу
+//? document.getElementsByTagName(); // выбор Коллекции элементов по Тегу
+//? document.getElementById(); // выбор одного элемента по ID
+
 
