dce8ecbb40ba699ed81731cd0e6704407bcaaec0
Igorplisko <igorplisko95@gmail.com>
2022-12-18 01:51:45 +0200

    innit

--- html-css-js/best-dom/js/3-dom-attr.js
+++ html-css-js/best-dom/js/3-dom-attr.js
@@ -9,3 +9,19 @@ toggleAttribute(name, force) — добавляет новый атрибут п
 hasAttributes() — возвращает true, если у элемента имеются какие-либо атрибуты
 getAttributesNames() — возвращает названия атрибутов элемента
 */
+
+const imgJs = document.querySelector('#logo-js');
+const altValue = imgJs.getAttribute('alt');
+imgJs.removeAttribute('alt');
+console.log(altValue);
+
+const img2 = document.querySelector('#logo-php');
+const srcValue2 = img2.getAttribute('src');
+img2.setAttribute('src', './img/php.png');
+img2.setAttribute('width', '400');
+// img2.src = './img/js.png'; //?  некоторые атрибуты можно менять через свойства
+// img2.width = '300'
+
+const buttonJs = document.querySelector('#button-js');
+// const setAttribute = buttonJs.setAttribute( "value", 'Измененная кнопка')
+buttonJs.value = 'Отправить';
