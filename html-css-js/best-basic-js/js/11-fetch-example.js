1c2545ae73b99d09af9d3ec7b9d736bdc4275413
Igorplisko <igorplisko95@gmail.com>
2022-12-25 19:01:53 +0200

    innit

--- html-css-js/best-basic-js/js/11-fetch-example.js
+++ html-css-js/best-basic-js/js/11-fetch-example.js
@@ -1,3 +1,7 @@
 // ====  Пример fetch c промисами получение данных по API   =======
 
-console.log('hey')
+console.log('hey');
+
+//! I did not write this code for certain reasons
+//JavaScript для начинающих 2022. Полный курс за 6 часов. Уроки. Теория + практика
+//!video time: 6 hr : 30 minutes  --- at this time there is a lesson on Fetch
